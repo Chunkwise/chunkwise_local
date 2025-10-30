134122390c538838d35b900b946446532b22b2dc
tylermcgraw <=>
2025-10-30 16:08:28 -0700

    Added Chonkie recursive chunker and LangChain token + recursive chunkers. Added separate routes for each chunker (this could be done with a single route but I modeled our future serverless architecture)

--- chunking/main.py
+++ chunking/main.py
@@ -1,23 +1,78 @@
 from fastapi import FastAPI
-from chonkie import TokenChunker
+from chonkie import TokenChunker, RecursiveChunker, RecursiveRules
+from langchain_text_splitters import TokenTextSplitter, RecursiveCharacterTextSplitter
 from pydantic import BaseModel
 
 app = FastAPI()
-chunker = TokenChunker(
-tokenizer="character", # Default tokenizer (or use "gpt2", etc.)
-chunk_size=10, # Maximum tokens per chunk
-chunk_overlap=0 # Overlap between chunks
-)
 
-class Item(BaseModel):
-    text: str
+# class LangChainChunk(BaseModel):  # LangChain chunks are just strings
+
+class ChonkieChunk(BaseModel):
+    text: str           # The chunk text
+    start_index: int    # Starting position in original text
+    end_index: int      # Ending position in original text
+    token_count: int    # Number of tokens in Chunk
 
 
-@app.get("/")
-def read_root():
-    return {"Hello World"}
+class LangChainItem(BaseModel):
+    text: str                     # Text to be chunked
+    chunk_size: int = 2048        # Maximum tokens per chunk
+    chunk_overlap: int = 128      # Overlap between chunks
 
-@app.post("/chunks")
-def chunk(item: Item):
+
+class ChonkieTokenItem(BaseModel):
+    text: str                     # Text to be chunked
+    tokenizer: str = "character"  # Default tokenizer (or use "gpt2", etc.)
+    chunk_size: int = 2048        # Maximum tokens per chunk
+    chunk_overlap: int = 128      # Overlap between chunks
+
+
+class ChonkieRecursiveItem(BaseModel):
+    text: str                                 # Text to be chunked
+    tokenizer: str = "character"              # Default tokenizer
+    chunk_size: int = 2048                    # Maximum tokens per chunk
+    rules: RecursiveRules = RecursiveRules()  # Rules to use for chunking
+    min_characters_per_chunk: int = 128       # Smallest chunk size
+
+
+@app.post("/chunks/token/chonkie")
+def chunk(item: ChonkieTokenItem) -> list[ChonkieChunk]:
+    chunker = TokenChunker(
+      tokenizer=item.tokenizer, 
+      chunk_size=item.chunk_size, 
+      chunk_overlap=item.chunk_overlap
+    )
     chunks = chunker(item.text)
+    return chunks
+
+
+@app.post("/chunks/recursive/chonkie")
+def chunk(item: ChonkieRecursiveItem) -> list[ChonkieChunk]:
+    chunker = RecursiveChunker(
+      tokenizer=item.tokenizer,
+      chunk_size=item.chunk_size,
+      rules=item.rules,
+      min_characters_per_chunk=item.min_characters_per_chunk
+    )
+    chunks = chunker(item.text)
+    return chunks
+
+
+@app.post("/chunks/token/langchain")
+def chunk(item: LangChainItem) -> list[str]:
+    chunker = TokenTextSplitter(
+      chunk_size=item.chunk_size, 
+      chunk_overlap=item.chunk_overlap
+    )
+    chunks = chunker.split_text(item.text)
+    return chunks
+
+
+@app.post("/chunks/recursive/langchain")
+def chunk(item: LangChainItem) -> list[str]:
+    chunker = RecursiveCharacterTextSplitter(
+      chunk_size=item.chunk_size,
+      chunk_overlap=item.chunk_overlap
+    )
+    chunks = chunker.split_text(item.text)
     return chunks
