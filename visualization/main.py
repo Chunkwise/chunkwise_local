1dddd8edcfc485e48e262b12b43bd5aaa6d12e42
tylermcgraw <=>
2025-11-04 15:46:42 -0800

    Fixed chunkwise_core import. Refactored utils.

--- visualization/main.py
+++ visualization/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from typing import List
-from viz import Visualizer
+from utils import Visualizer, save_to_file
 from chunkwise_core import Chunk
 
 app = FastAPI()
@@ -14,25 +14,6 @@ def visualize(chunks: List[Chunk]) -> str:
     """
     viz = Visualizer()
     html_content = viz.get_HTML(chunks)
-    # For testing purposes only
+    # For testing purposes only. Saves to ./example_visualizations/visualization.html
     # save_to_file(html_content)
     return html_content
-
-
-def save_to_file(html_content: str) -> None:
-    """
-    For testing purposes only
-    Receives HTML as a string
-    Saves an html file
-    """
-    try:
-        import os
-
-        filepath = os.path.abspath("example_visualizations/visualization.html")
-        with open(filepath, "w", encoding="utf-8") as f:
-            f.write(html_content)
-        print(f"HTML visualization saved to: file://{filepath}")
-    except IOError as e:
-        raise IOError(f"Error: Could not write file '{filename}': {e}")
-    except Exception as e:
-        raise Exception(f"An unexpected error occurred during file saving: {e}")
