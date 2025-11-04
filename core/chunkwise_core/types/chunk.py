210e4e1cca98498aec8814fc8003a94fb7c16ab4
tylermcgraw <=>
2025-11-04 07:57:27 -0800

    Added 'core' package. Currently has types, but should also be used to store constants, helpers, or anything shared between services

---
+++ core/chunkwise_core/types/chunk.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Optional
+
+
+class Chunk(BaseModel):
+    text: str  # The chunk text
+    start_index: int  # Starting position in original text
+    end_index: int  # Ending position in original text
+    token_count: Optional[int] = None  # Number of tokens in Chunk
