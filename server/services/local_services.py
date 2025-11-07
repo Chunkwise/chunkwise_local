8ebc26056e87b6ece8f1145caf57fa5979730430
Benjamin Stevens <B3nbinoh@gmail.com>
2025-11-07 14:32:49 -0700

    #50 Create files for services

---
+++ server/services/local_services.py
@@ -0,0 +1,10 @@
+def get_chunks():
+    pass
+
+
+def get_visualization():
+    pass
+
+
+def get_evaluation():
+    pass
---
+++ server/services/s3_services.py
@@ -0,0 +1,14 @@
+def upload_s3_file():
+    pass
+
+
+def download_s3_file():
+    pass
+
+
+def delete_s3_file():
+    pass
+
+
+def get_s3_file_names():
+    pass
