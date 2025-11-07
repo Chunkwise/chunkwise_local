fe134c68b6ea13ffeebd1c23bc236af732b8700a
Benjamin Stevens <B3nbinoh@gmail.com>
2025-11-07 14:56:59 -0700

    Fixed linting errors in s3 services

--- server/services/s3_services.py
+++ server/services/s3_services.py
@@ -1,41 +1,51 @@
+"""
+This file contains the list of services involving s3. This includes:
+uploading a file, downloading a file, deleting a file, and getting the
+ids of files in a bucket.
+"""
+
+import logging
 import boto3
 from botocore.exceptions import ClientError
-import logging
 
 BUCKET_NAME = "chunkwise-test"
 
 
 async def upload_s3_file(document_id):
-    # Upload a file to s3
+    """Upload a file to s3"""
     try:
         s3_client = boto3.client("s3")
         s3_client.upload_file(f"documents/{document_id}", BUCKET_NAME, document_id)
+        return True
 
-    except ClientError as e:
+    except ClientError:
         logging.exception("s3 ClientError while uploading document")
 
 
 async def download_s3_file(document_id):
-    # Download a file from s3
+    """Download a file from s3"""
     try:
         s3_client = boto3.client("s3")
         s3_client.download_file(BUCKET_NAME, document_id, f"documents/{document_id}")
-    except ClientError as e:
+        return True
+
+    except ClientError:
         logging.exception("s3 ClientError while downloading document")
 
 
 async def delete_s3_file(document_id):
-    # Delete a file on s3
+    """Delete a file on s3"""
     try:
         s3_client = boto3.client("s3")
         s3_client.delete_object(Key=document_id, Bucket=BUCKET_NAME)
+        return True
 
-    except ClientError as e:
-        logging.exception("s3 ClientError while uploading document")
+    except ClientError:
+        logging.exception("s3 ClientError while deleting document")
 
 
 async def get_s3_file_names():
-    # Get the list of resources from a bucket
+    """Get the list of resources from a bucket"""
     try:
         s3_client = boto3.client("s3")
         resources = s3_client.list_objects_v2(Bucket=BUCKET_NAME)
@@ -43,5 +53,5 @@ async def get_s3_file_names():
         file_names = [resource["Key"] for resource in resources["Contents"]]
         return file_names
 
-    except ClientError as e:
-        logging.exception("s3 ClientError while uploading document")
+    except ClientError:
+        logging.exception("s3 ClientError while getting document names")
