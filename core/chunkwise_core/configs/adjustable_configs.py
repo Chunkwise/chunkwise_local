743e3c775739d74d611e32b0ef65e57b16e61d09
tylermcgraw <=>
2025-11-10 15:59:49 -0800

    Fixed adjustable_configs to all have

--- core/chunkwise_core/configs/adjustable_configs.py
+++ core/chunkwise_core/configs/adjustable_configs.py
@@ -1,19 +1,21 @@
 adjustable_configs = [
     {
         "name": "Chonkie Token",
-        "chunk_size": {"default": 2048, "min": 1, "max": 8192},
-        "chunk_overlap": {"default": 0, "min": 0, "max": 4086},
+        "chunk_size": {"type": "int", "default": 2048, "min": 1, "max": 8192},
+        "chunk_overlap": {"type": "int", "default": 0, "min": 0, "max": 4086},
     },
     {
         "name": "Chonkie Sentence",
-        "chunk_size": {"default": 2048, "min": 1, "max": 8192},
-        "chunk_overlap": {"default": 0, "min": 0, "max": 4086},
+        "chunk_size": {"type": "int", "default": 2048, "min": 1, "max": 8192},
+        "chunk_overlap": {"type": "int", "default": 0, "min": 0, "max": 4086},
         "min_sentences_per_chunk": {
+            "type": "int",
             "default": 1,
             "min": 1,
             "max": 100,
         },
         "min_characters_per_sentence": {
+            "type": "int",
             "default": 12,
             "min": 1,
             "max": 100,
@@ -21,33 +23,37 @@ adjustable_configs = [
     },
     {
         "name": "Chonkie Recursive",
-        "chunk_size": {"default": 2048, "min": 1, "max": 8192},
+        "chunk_size": {"type": "int", "default": 2048, "min": 1, "max": 8192},
         "min_characters_per_chunk": {
+            "type": "int",
             "default": 24,
             "min": 1,
-            "max": 100,
+            "max": 8192,
         },
     },
     {
         "name": "Chonkie Slumber",
-        "chunk_size": {"default": 2048, "min": 1, "max": 8192},
+        "chunk_size": {"type": "int", "default": 2048, "min": 1, "max": 8192},
         "min_characters_per_chunk": {
+            "type": "int",
             "default": 24,
             "min": 1,
-            "max": 100,
+            "max": 8192,
         },
     },
     {
         "name": "Chonkie Semantic",
-        "chunk_size": {"default": 2048, "min": 1, "max": 8192},
-        "threshold": {"default": 0.8, "min": 0, "max": 1},
-        "similarity_window": {"default": 3, "min": 1, "max": 100},
+        "chunk_size": {"type": "int", "default": 2048, "min": 1, "max": 8192},
+        "threshold": {"type": "float", "default": 0.8, "min": 0, "max": 1},
+        "similarity_window": {"type": "int", "default": 3, "min": 1, "max": 100},
         "min_sentences_per_chunk": {
+            "type": "int",
             "default": 1,
             "min": 1,
             "max": 100,
         },
         "min_characters_per_sentence": {
+            "type": "int",
             "default": 12,
             "min": 1,
             "max": 100,
@@ -55,17 +61,17 @@ adjustable_configs = [
     },
     {
         "name": "LangChain Token",
-        "chunk_size": {"default": 2048, "min": 1, "max": 8192},
-        "chunk_overlap": {"default": 0, "min": 0, "max": 4086},
+        "chunk_size": {"type": "int", "default": 2048, "min": 1, "max": 8192},
+        "chunk_overlap": {"type": "int", "default": 0, "min": 0, "max": 4086},
     },
     {
         "name": "LangChain Recursive",
-        "chunk_size": {"default": 2048, "min": 1, "max": 8192},
-        "chunk_overlap": {"default": 0, "min": 0, "max": 4086},
+        "chunk_size": {"type": "int", "default": 2048, "min": 1, "max": 8192},
+        "chunk_overlap": {"type": "int", "default": 0, "min": 0, "max": 4086},
     },
     {
         "name": "LangChain Character",
-        "chunk_size": {"default": 2048, "min": 1, "max": 8192},
-        "chunk_overlap": {"default": 0, "min": 0, "max": 4086},
+        "chunk_size": {"type": "int", "default": 2048, "min": 1, "max": 8192},
+        "chunk_overlap": {"type": "int", "default": 0, "min": 0, "max": 4086},
     },
 ]
