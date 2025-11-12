3711de84caffb6c42a21fc7e7a95b7baf80d1d94
Benjamin Stevens <B3nbinoh@gmail.com>
2025-11-12 13:19:54 -0700

    When chunking_strategy or document_title change clear the row

--- server/main.py
+++ server/main.py
@@ -195,6 +195,17 @@ async def insert_workflow(body: dict = Body(...)):
 @handle_endpoint_exceptions
 async def change_workflow(workflow_id: int, workflow_update: Workflow = Body(...)):
     update_dict = workflow_update.__dict__
+
+    if (
+        not workflow_update["chunking_strategy"] is None
+        or not workflow_update["document_title"] is None
+    ):
+        workflow_update["chunks_stats"] = ""
+        workflow_update["visualization_html"] = ""
+        workflow_update["evaluation_metrics"] = ""
+    if not workflow_update["document_title"] is None:
+        workflow_update["chunking_strategy"] = ""
+
     result = update_workflow(workflow_id, update_dict)
     return result
 
