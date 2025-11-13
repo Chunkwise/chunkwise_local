7ad6fc0ebeca3dc626c2d760e8231fe0b5ee4aa3
Benjamin Stevens <B3nbinoh@gmail.com>
2025-11-12 17:16:04 -0700

    Fix get all documents again

--- server/services/s3_services.py
+++ server/services/s3_services.py
@@ -65,7 +65,7 @@ async def get_s3_file_names():
 
         # Create a list of the files names of a bucket, remove the beginning path
         file_names = [
-            resource["Key"].lstrip("documents/").rstrip(".txt")
+            resource["Key"].replace("documents/", "").replace(".txt", "")
             for resource in resources["Contents"]
         ]
         return file_names
