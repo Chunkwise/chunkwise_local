3d4828e0e1e308c8afef401564720775a234ca34
Benjamin Stevens <B3nbinoh@gmail.com>
2025-11-15 11:45:12 -0700

    Separated host and port variables for chunkwise_services.
    Added logging for sending requests
    
    Moved scheme into string literal

--- server/services/chunkwise_services.py
+++ server/services/chunkwise_services.py
@@ -5,11 +5,16 @@ Contains service calls to the services implemented by the chunkwise team.
 import os
 import json
 import requests
+import dotenv
 from server_types import EvaluationResponse, Chunk
 
 
-CHUNKING_SERVICE_URL = os.getenv("CHUNKING_SERVICE_URL", "http://localhost:8001")
-EVALUATION_SERVICE_URL = os.getenv("EVALUATION_SERVICE_URL", "http://localhost:8003")
+dotenv.load_dotenv()
+
+CHUNKING_SERVICE_HOST = os.getenv("CHUNKING_SERVICE_HOST", "localhost")
+CHUNKING_SERVICE_PORT = os.getenv("CHUNKING_SERVICE_PORT", 1111)
+EVALUATION_SERVICE_HOST = os.getenv("EVALUATION_SERVICE_HOST", "localhost")
+EVALUATION_SERVICE_PORT = os.getenv("EVALUATION_SERVICE_PORT", 2222)
 
 
 async def get_chunks(chunker_config, document) -> list[Chunk]:
@@ -24,8 +29,13 @@ async def get_chunks(chunker_config, document) -> list[Chunk]:
     }
 
     # Send request to chunking service
+    print(
+        f"Sending request to http://{CHUNKING_SERVICE_HOST}:{CHUNKING_SERVICE_PORT}/chunk_with_metadata"
+    )
     chunking_response = requests.post(
-        f"{CHUNKING_SERVICE_URL}/chunk_with_metadata", json=request_body, timeout=10
+        f"http://{CHUNKING_SERVICE_HOST}:{CHUNKING_SERVICE_PORT}/chunk_with_metadata",
+        json=request_body,
+        timeout=10,
     )
     chunking_response.raise_for_status()
     chunks: list[Chunk] = [Chunk(**c) for c in chunking_response.json()]
@@ -43,8 +53,13 @@ async def get_evaluation(chunker_config, document_id) -> EvaluationResponse:
     }
 
     # Send request to chunking service
+    print(
+        f"Sending request to http://{EVALUATION_SERVICE_HOST}:{EVALUATION_SERVICE_PORT}/evaluate"
+    )
     evaluation_response = requests.post(
-        f"{EVALUATION_SERVICE_URL}/evaluate", json=request_body, timeout=240
+        f"http://{EVALUATION_SERVICE_HOST}:{EVALUATION_SERVICE_PORT}/evaluate",
+        json=request_body,
+        timeout=240,
     )
     evaluation_response.raise_for_status()
     evaluation_json: EvaluationResponse = evaluation_response.json()
