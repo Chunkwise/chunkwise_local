4510a26760714b1e4cb39ad130ceadaf1097e53c
tylermcgraw <=>
2025-11-18 15:15:55 -0800

    Added schema for deployment RDS

---
+++ embeddings_schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS deployment;
+
+DROP TABLE IF EXISTS chunk;
+
+CREATE TABLE deployment (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  created_at timestamptz NOT NULL DEFAULT NOW(),
+  chunker_config TEXT NOT NULL,
+  deployment_status TEXT
+);
+
+CREATE TABLE chunk (
+  id SERIAL PRIMARY KEY,
+  deployment_id INT REFERENCES deployment(id),
+  document_key TEXT NOT NULL,
+  chunk_id INT NOT NULL,
+  chunk_text TEXT NOT NULL,
+  embedding VECTOR(1536) NOT NULL
+);
