e6c199f28870639976fe23f8e11599c627f0a65f
tylermcgraw <=>
2025-11-19 11:36:11 -0800

    Updated schema for vector db (1 table instead of 2)

--- vector_schema.sql
+++ vector_schema.sql
@@ -1,20 +1,9 @@
-DROP TABLE IF EXISTS deployment;
-
-DROP TABLE IF EXISTS chunk;
+DROP TABLE IF EXISTS workflow_1;
 
 CREATE EXTENSION IF NOT EXISTS vector;
 
-CREATE TABLE deployment (
+CREATE TABLE workflow_1 (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  created_at timestamptz NOT NULL DEFAULT NOW(),
-  chunker_config TEXT NOT NULL,
-  deployment_status TEXT
-);
-
-CREATE TABLE chunk (
-  id SERIAL PRIMARY KEY,
-  deployment_id INT REFERENCES deployment(id),
   document_key TEXT NOT NULL,
   chunk_id INT NOT NULL,
   chunk_text TEXT NOT NULL,
