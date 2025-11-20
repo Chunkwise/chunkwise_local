55189b1173e8375046182f0be1ddddc830c0b747
Benjamin Stevens <B3nbinoh@gmail.com>
2025-11-20 14:51:24 -0700

    #198 Fixed pyproject, added function skeletons, created logo file

---
+++ cli/cli.py
@@ -0,0 +1,44 @@
+import typer
+from rich import print
+
+app = typer.Typer()
+
+
+def display_logo():
+    pass
+
+
+@app.command()
+def deploy(openai_api_key: str, region: str, confirm: bool):
+    if confirm:
+        print(f"creating VPC in {region}...")
+        print(f"creating secret {openai_api_key}...")
+        print("AWS Stack deployed")
+    else:
+        print("deployment cancelled")
+
+
+@app.command()
+def destroy(include_rds: bool = False):
+    print("AWS Stack destroyed")
+
+    if include_rds:
+        print("RDS instance destroyed")
+
+
+@app.command()
+def client_build():
+    print("building the client...")
+    print("client built")
+
+
+@app.command()
+def client_start():
+    print("starting the client...")
+    print("client running")
+
+
+@app.command()
+def client():
+    client_build()
+    client_start()
