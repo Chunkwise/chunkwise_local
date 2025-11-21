1dab5786574fe39ccb441db28895ce8987dd4f63
Xiran Lu <xlu92427@gmail.com>
2025-11-21 10:42:26 -0800

    Forgot to add updated schema

--- vector_schema.sql
+++ vector_schema.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION IF NOT EXISTS vector;
 CREATE TABLE workflow_1 (
   id SERIAL PRIMARY KEY,
   document_key TEXT NOT NULL,
-  chunk_id INT NOT NULL,
+  chunk_index INT NOT NULL,
   chunk_text TEXT NOT NULL,
   embedding VECTOR(1536) NOT NULL
 );
